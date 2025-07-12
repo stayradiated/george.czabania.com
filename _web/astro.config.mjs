4d41dcfd727fd5076760cc5e11d580370d8161cf
George Czabania <1140167+stayradiated@users.noreply.github.com>
2025-07-12 15:56:22 +0200

    fix: update base path for site and blog loader configuration 🛠️
    This commit updates the base path in the Astro configuration file and adjusts the blog loader pattern to accurately reflect the new structure. These changes ensure that the correct resources are accessed and improve the organization of the project.
    
    - Changed the base path from '/obsidian' to '/obsidian-public' in `astro.config.mjs`.
    - Updated the blog loader pattern to include both 'notes' and 'posts' directories in `config.ts`.
    
    These changes were made to align the configuration with the updated project structure and to support better organization of content files.

--- _web/astro.config.mjs
+++ _web/astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stayradiated.github.io',
-  base: '/obsidian',
+  base: '/obsidian-public',
   integrations: [sitemap()],
   output: 'static',
 });
--- _web/src/content/config.ts
+++ _web/src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: '../public-blog' }),
+  loader: glob({ pattern: '{notes,posts}/**/*.md', base: '../' }),
   schema: z.object({
     title: z.string(),
     date: z.string().transform(str => new Date(str)),
